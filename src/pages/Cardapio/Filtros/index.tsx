549599605e8ff8f1362cbb54e65c1583ab4f110c
YuriOnorio <yronorio@gmail.com>
2023-10-05 11:46:52 -0300

    .

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -1,26 +1,29 @@
-import styles from './Filtros.module.scss';
-import filtros from './filtros.json'
+import styles from "./Filtros.module.scss";
+import filtros from "./filtros.json";
 
-type IOpcao = typeof filtros[0]
+type IOpcao = (typeof filtros)[0];
 
 interface Props {
-    filtro: number | null;
-    setFiltro: React.Dispatch<React.SetStateAction<number | null>>
+  filtro: number | null;
+  setFiltro: React.Dispatch<React.SetStateAction<number | null>>;
 }
 
-
-export default function Filtros({filtro, setFiltro}: Props) {
-    function selecionarFiltro(opcao: IOpcao) {
-        return setFiltro(opcao.id);
-    }
-    return(
-        <div className={styles.filtro}>
-            {filtros.map((opcao) =>
-                <button className={styles.filtro__filtro} 
-                key={opcao.id}
-                onClick={() => selecionarFiltro(opcao)}>
-                </button>
-            )}
-        </div>
-    )
+export default function Filtros({ filtro, setFiltro }: Props) {
+  function selecionarFiltro(opcao: IOpcao) {
+    return setFiltro(opcao.id);
+  }
+  return (
+    <div className={styles.filtro}>
+      {filtros.map((opcao) => (
+        <button
+          className={`
+          ${styles.filtro__filtro}
+          ${filtro === opcao.id ? styles["filtro__filtro--ativo"] : ""}
+          `}
+          key={opcao.id}
+          onClick={() => selecionarFiltro(opcao)}
+        ></button>
+      ))}
+    </div>
+  );
 }
